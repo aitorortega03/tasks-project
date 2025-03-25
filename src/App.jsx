4e738de91db4c11546738fac1322169e47d796d7
aitorortega03 <aitorortega21@gmail.com>
2025-03-25 20:37:47 +0100

    Add some styles and page skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,47 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>React App</h1>  
-    </>
+    <div className='page'>
+      <header>
+        <h1>Tasks Project</h1>
+      </header>
+      <main>
+
+        <section>
+          <form>
+            <input
+              type="text"
+              placeholder="Add new task"
+            />
+            <button type="submit">Addd task</button>
+          </form>
+        </section>
+
+        <section>
+          <button >Todas</button>
+          <button >Activas</button>
+          <button >Completadas</button>
+        </section>
+
+        <section>
+          {/* Task list */}
+        </section>
+
+        <section>
+          <h2>Productivity Stats</h2>
+          <div>
+            <div>
+              <span>Completed tasks</span>
+              <span>0</span>
+            </div>
+            <div>
+              <span>Porcentaje de Progreso</span>
+              <span>0%</span>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   )
 }
 
