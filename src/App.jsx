03f1cc1bfa18a7ab5995d05ca86452702126d5c0
aitorortega03 <aitorortega21@gmail.com>
2025-03-26 20:28:46 +0100

    Add tasks examples and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 
+const tasksExamle = [
+  { id: 1, text: 'Learn React', completed: false },
+  { id: 2, text: 'Learn Firebase', completed: false },
+  { id: 3, text: 'Learn CSS', completed: false }
+]
+
 function App() {
+  const [tasks, setTasks] = useState([])
 
   return (
     <div className='page'>
@@ -15,29 +23,41 @@ function App() {
               type="text"
               placeholder="Add new task"
             />
-            <button type="submit">Addd task</button>
+            <button type="submit">Add task</button>
           </form>
         </section>
 
         <section>
-          <button >Todas</button>
-          <button >Activas</button>
-          <button >Completadas</button>
+          <button>Todas</button>
+          <button>Activas</button>
+          <button>Completadas</button>
         </section>
 
         <section>
           {/* Task list */}
+          {tasks ?
+            <ul>
+              {tasks.map(task => (
+                <li key={task.id}>
+                  <input type="checkbox" />
+                  <span>{task.text}</span>
+                  <button>Delete</button>
+                </li>
+              ))}
+            </ul>
+            : <p>No tasks</p>
+          }
         </section>
 
         <section>
           <h2>Productivity Stats</h2>
           <div>
             <div>
-              <span>Completed tasks</span>
+              <span>Completed tasks </span>
               <span>0</span>
             </div>
             <div>
-              <span>Porcentaje de Progreso</span>
+              <span>Porcentaje de Progreso </span>
               <span>0%</span>
             </div>
           </div>
